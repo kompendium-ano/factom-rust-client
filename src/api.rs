903490e95c769dd22f3e935b1ff500f8d32d4292
MitchellBerry <mitchellberry@protonmail.com>
2018-07-06 07:51:29 +0800

    ablock height

--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,13 @@
 use super::*;
 
 impl Factomd{
+
     pub fn ablock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
         let mut params = HashMap::new();
         params.insert("height".to_string(), json!(height));
-        let json = ApiRequest::method("ablock-by-height")
+        self.api_call(ApiRequest::method("ablock-by-height")
                                 .parameters(params)
-                                .to_json();
-        api_call(json, self.uri())
+                                .to_json())
     }
 
 }
