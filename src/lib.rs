47e51474db415de40482878fdf7f9bde863cfbc0
MitchellBerry <mitchellberry@protonmail.com>
2018-07-05 18:39:12 +0800

    moved tests and api into seperate files

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ use hyper_tls::HttpsConnector;
 use serde_json::{Value, json};
 use tokio::prelude::*;
 
+mod tests;
+mod api;
+
 const JSONRPC : &str = "2.0";
 const ID: u32 = 0;
 const DAEMON_PORT: u16 = 8088;
@@ -128,14 +131,6 @@ impl Factomd {
             .expect("Unable to build URI from Factomd struct")
     }
 
-    fn ablock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
-        let mut params = HashMap::new();
-        params.insert("height".to_string(), json!(height));
-        let json = ApiRequest::method("ablock-by-height")
-                                .parameters(params)
-                                .to_json();
-        api_call(json, self.uri())
-    }
 }
 
     fn api_call(json_str: String, uri: Uri)->  impl Future<Item=Response, Error=FetchError> {
@@ -164,36 +159,3 @@ impl Factomd {
     }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*; 
-
-    fn factomd()-> Factomd{
-        let mut factomd = Factomd::new();
-        factomd.port(443);
-        factomd.https();
-        factomd
-    }
-
-    fn get_result<F, R, E>(fut: F)-> Result<R, E>
-        where
-            F: Send + 'static + Future<Item = R, Error = E>,
-            R: Send + 'static,
-            E: Send + 'static,
-        {
-            let mut runtime = tokio::runtime::Runtime::new().expect("Unable to create a tokio runtime");
-            runtime.block_on(fut)
-        }
-
-    #[test]
-    fn ablock_by_height() {
-        let query = factomd().ablock_by_height(14460)
-                                .map(|result| {
-                                    result.result["ablock"]["backreferencehash"].clone()
-                                })
-                                .map_err(|err| err);
-        let expected = "0a9aa1efbe7d0e8d9c1d460d1c78e3e7b50f984e65a3f3ee7b73100a94189dbf";
-        let result = get_result(query);
-        assert_eq!(result.unwrap(), expected);
-    }
-}
