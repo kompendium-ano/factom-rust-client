cb57bda58ec1f2b4c6f01a6bb200a8daba06e4b9
MitchellBerry <mitchellberry@protonmail.com>
2018-07-06 08:02:37 +0800

    ack

--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,25 @@
 use super::*;
 
 impl Factomd{
-
     pub fn ablock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
         let mut params = HashMap::new();
         params.insert("height".to_string(), json!(height));
         self.api_call(ApiRequest::method("ablock-by-height")
                                 .parameters(params)
                                 .to_json())
+    }
+
+    pub fn ack(self, hash: &str, chainid: &str, full_transaction: Option<&str>)
+                                    -> impl Future<Item=Response, Error=FetchError> {
+        let mut params = HashMap::new();
+        params.insert("hash".to_string(), json!(hash));
+        params.insert("chainid".to_string(), json!(chainid));
+        if let Some(tx) = full_transaction{
+            params.insert("fulltransaction".to_string(), json!(tx));
+        }
+        self.api_call(ApiRequest::method("ack")
+                                    .parameters(params)
+                                    .to_json())
     }
 
 }
