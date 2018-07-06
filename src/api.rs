00e66eaceefa389d677809028edea66bd61db916
MitchellBerry <mitchellberry@protonmail.com>
2018-07-06 08:53:47 +0800

    ecblock height

--- src/api.rs
+++ src/api.rs
@@ -82,6 +82,12 @@ impl Factomd{
                                 .to_json())
     }
 
-
+    pub fn ecblock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
+        let mut params = HashMap::new();
+        params.insert("height".to_string(), json!(height));
+        self.api_call(ApiRequest::method("ecblock-by-height")
+                                .parameters(params)
+                                .to_json())
+    }
 
 }
