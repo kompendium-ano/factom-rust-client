0e82b98fbbc9a3fad7ccc01bb8788d6b1ea26e2e
MitchellBerry <mitchellberry@protonmail.com>
2018-07-06 08:46:23 +0800

    dblock

--- src/api.rs
+++ src/api.rs
@@ -59,4 +59,23 @@ impl Factomd{
                                 .to_json())
     }
 
+
+    pub fn dblock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
+        let mut params = HashMap::new();
+        params.insert("height".to_string(), json!(height));
+        self.api_call(ApiRequest::method("dblock-by-height")
+                                .parameters(params)
+                                .to_json())
+    }
+
+
+    pub fn directory_block(self, keymr: &str)-> impl Future<Item=Response, Error=FetchError>{
+        let mut params = HashMap::new();
+        params.insert("keymr".to_string(), json!(keymr));
+        self.api_call(ApiRequest::method("directory-block")
+                                .parameters(params)
+                                .to_json())
+    }
+
+
 }
