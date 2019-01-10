200bf47569ffb26433cf26b36c034092fda553d4
MitchellBerry <mitchellberry@protonmail.com>
2019-01-10 15:26:17 +0800

    remove print debug

--- src/api.rs
+++ src/api.rs
@@ -11,7 +11,7 @@ pub fn str_to_hex(utf8: &str) -> String {
 
 impl Factomd{
 
-    
+
     pub fn ablock_by_height(self, height: u32)-> impl Future<Item=Response, Error=FetchError>{
         let mut params = HashMap::new();
         params.insert("height".to_string(), json!(height));
@@ -116,7 +116,6 @@ impl Factomd{
         let json = ApiRequest::method("entry-block")
                                 .parameters(params)
                                 .to_json();
-        dbg!(&json);
         self.api_call(json)
     }
 
