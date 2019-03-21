aff54aa6518941200883cdf7e448c57cab191489
MitchellBerry <mitchellberry@protonmail.com>
2019-03-21 15:42:29 +0800

    moved errors into seperate module

---
+++ src/errors.rs
@@ -0,0 +1,40 @@
+use super::*;
+/**
+Module containing errors from networking, json parsing or the API.
+Networking errors are handled by [hyper](https://docs.rs/hyper/0.12.25/hyper/)
+Problems parsing JSON by [serde_json](https://docs.serde.rs/serde_json/error/index.html)
+*/
+#[derive(Debug)]
+pub enum FetchError {
+    Http(hyper::Error),
+    Json(serde_json::Error),
+}
+
+impl From<hyper::Error> for FetchError {
+    fn from(err: hyper::Error) -> FetchError {
+        FetchError::Http(err)
+    }
+}
+
+impl From<serde_json::Error> for FetchError {
+    fn from(err: serde_json::Error) -> FetchError {
+        FetchError::Json(err)
+    }
+}
+
+struct ApiError {
+    code: i16,
+    message: &'static str
+}
+
+/// [Potential API errors](https://docs.factom.com/api?json#errors) returned by the server are enumerated by FactomError 
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum FactomError {
+    ParseError,
+    InvalidRequest,
+    InvalidParams,
+    InternalError,
+    MethodNotFound,
+    RepeatedCommit,
+    UndefinedError
+}
