f1c3ec4602d2a421e35a5f4fb6449e4bfedede5a
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-10-17 15:42:17 +0800

    moved factom struct to requests refactored inner api call

--- src/requests.rs
+++ src/requests.rs
@@ -1,14 +1,31 @@
 use super::*;
+use hyper::client::HttpConnector;
+use http::request::Builder;
 
+type HttpsClient = Client<HttpsConnector<HttpConnector>, hyper::Body>;
+
+/// Main struct from which API requests are built
+// #[derive(Clone)]
+pub struct Factom{
+  pub client: HttpsClient,
+  pub factomd_req: Builder,
+  pub uri: &'static str,
+  pub wallet_uri: &'static str,
+  pub id: u32
+}
+
+ 
 impl Factom {
   pub fn call(self, method: &str, params: HashMap<String, Value>)
             ->  impl Future<Item=Response, Error=FetchError> {
-      self.inner_call(method, params, self.uri)
+      let uri = self.uri.clone();
+      self.inner_call(method, params, uri)
   }
 
   pub fn walletd_call(self, method: &str, params: HashMap<String, Value>)
             ->  impl Future<Item=Response, Error=FetchError>{
-      self.inner_call(method, params, self.wallet_uri)
+      let uri = self.wallet_uri.clone();
+      self.inner_call(method, params, uri)
   }
 
   pub fn debug_call(self, method: &str, params: HashMap<String, Value>)
@@ -17,42 +34,47 @@ impl Factom {
   }
 
   fn inner_call(
-      self, 
+      mut self, 
       method: &str, 
       params: HashMap<String, Value>, 
       uri: &str
-    ) ->  impl Future<Item=Response, Error=FetchError> 
-    {
-      let json_str = ApiRequest::method(method)
-                                .parameters(params)
-                                .id(self.id)
-                                .to_json();
-      let mut req = Request::new(Body::from(json_str));
-      *req.method_mut() = Method::POST;
-      *req.uri_mut() = uri.parse()
-                          .unwrap_or_else(|_| 
-                            panic!("Parsing URI: {}", uri)
-                          );
-      req.headers_mut().insert(
-        hyper::header::CONTENT_TYPE,
-        HeaderValue::from_static("application/json")
-      );
+  ) ->  impl Future<Item=Response, Error=FetchError> 
+  {
+    let json_str = ApiRequest::method(method)
+                              .parameters(params)
+                              .id(self.id)
+                              .to_json();
+    // let mut req = Request::new(Body::from(json_str));
+    // *req.method_mut() = Method::POST;
+    // *req.uri_mut() = uri.parse()
+    //                     .unwrap_or_else(|_| 
+    //                       panic!("Parsing URI: {}", uri)
+    //                     );
+    // req.headers_mut().insert(
+    //   hyper::header::CONTENT_TYPE,
+    //   HeaderValue::from_static("application/json")
+    // );
 
-    // Use a https connector instead of the default
-    let https = HttpsConnector::new(4).expect("TLS initialization");
+  // // Use a https connector instead of the default
+  // let https = HttpsConnector::new(4).expect("TLS initialization");
 
-    // Hyper client
-    let client = Client::builder().build::<_, hyper::Body>(https);
-    
-    client.request(req)
-          .and_then(|res| 
-            {res.into_body().concat2()})
-          .from_err::<FetchError>()
-          .and_then(|json| 
-            {
-              let output: Response = serde_json::from_slice(&json)?;
-              Ok(output)
-            }
-          )
+  // // Hyper client
+  // let client = Client::builder().build::<_, hyper::Body>(https);
+
+  // This should serialise directly into the body
+  let req = self.factomd_req.body(Body::from(json_str)).unwrap();
+
+  self.client.request(req)
+        .and_then(|res| {res.into_body().concat2()})
+        .from_err::<FetchError>()
+        .and_then(|json| 
+          {
+            let output: Response = serde_json::from_slice(&json)?;
+            Ok(output)
+          }
+        )
   }
 }
+
+
+
