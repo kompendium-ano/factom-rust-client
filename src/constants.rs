de395f26a659e892add5a95a2d2737d1360dbc19
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-11-15 15:15:53 +1100

    use api version as a string instead

--- src/constants.rs
+++ src/constants.rs
@@ -1,15 +1,15 @@
 /// Default factom-walletd location
-pub const WALLETD_DEFAULT: &str = "http://localhost:8089/v2";
+pub const WALLETD_DEFAULT: &str = "http://localhost:8089";
 /// Default factomd location
-pub const FACTOMD_DEFAULT: &str = "http://localhost:8088/v2";
+pub const FACTOMD_DEFAULT: &str = "http://localhost:8088";
 /// Public factomd mainnet open node. For more information visit https://factomd.net
-pub const OPENNODE_URI: &str = "https://api.factomd.net/v2";
+pub const OPENNODE_URI: &str = "https://api.factomd.net";
 /// Public factomd testnet open node
-pub const DEV_OPENNODE_URI: &str = "https://dev.factomd.net/v2";
+pub const DEV_OPENNODE_URI: &str = "https://dev.factomd.net";
 /// Factomd debug functions
-pub const DEBUG_DEFAULT: &str = "http://localhost:8088/debug";
+pub const DEBUG: &str = "debug";
 /// Factom api versioning
-pub const API_VERSION: u8 = 2;
+pub const API_VERSION: &str = "v2";
 /// JSON-RPC versioning
 pub const JSONRPC : &str = "2.0";
 /// Default JSON-RPC ID
