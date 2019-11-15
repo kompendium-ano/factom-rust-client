6dfee76a6a85f5ece293ca9f198b1b1f78c952e6
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-11-15 15:12:03 +1100

    request module refactor

--- src/requests.rs
+++ src/requests.rs
@@ -1,26 +1,18 @@
 use super::*; 
 use constants::*;
 use serde_json::Value;
-use hyper_tls::HttpsConnector;
 use std::collections::HashMap;
+use std::num::Wrapping;
 use futures_util::TryStreamExt;
 use crate::responses::ApiResponse;
 use serde::{Serialize, de::DeserializeOwned};
-use http::{Uri, request::Builder, header::CONTENT_TYPE};
-use hyper::{Request, Chunk, Body, Client, client::ResponseFuture};
-
-
-enum RequestType{
-  Factomd,
-  Walletd,
-  Debug
-}
+use hyper::{Chunk, Body, client::ResponseFuture};
 
 /// Generic request struct is serialized into the JSON body 
 #[derive(Serialize, Debug)]
 pub struct ApiRequest {
   pub jsonrpc: &'static str,
-  pub id: usize,
+  pub id: Wrapping<usize>,
   pub method: String,
   pub params: HashMap<String, Value>
 }
@@ -30,7 +22,7 @@ impl ApiRequest {
   pub fn new(method: &str) -> ApiRequest {
     ApiRequest {
       jsonrpc: JSONRPC,
-      id: ID,
+      id: Wrapping(ID),
       method: method.to_string(),
       params: HashMap::new()
     }
@@ -43,71 +35,36 @@ impl ApiRequest {
   }
 }
 
-/// Main struct from which API requests are built
-pub struct Factom{
-  pub client: HttpsClient,
-  pub factomd: Builder,
-  pub walletd: Builder,
-  pub factomd_uri: Uri,
-  pub walletd_uri: Uri,
-  pub id: usize
+pub enum RequestType{
+  Factomd,
+  Walletd,
+  Debug
 }
 
-impl Clone for Factom {
-  fn clone(&self) -> Self {
-    let client = self.client.clone();
-    let factomd = request_builder(self.factomd_uri.clone());
-    let walletd = request_builder(self.walletd_uri.clone());
-    Factom {
-      client,
-      factomd,
-      walletd,
-      factomd_uri: self.factomd_uri.clone(),
-      walletd_uri: self.walletd_uri.clone(),
-      id: self.id
-    }
-  }
-}
- 
 impl Factom {
-
-  pub fn new() -> Factom {
-    let factomd_uri: Uri = FACTOMD_DEFAULT.parse().unwrap(); 
-    let walletd_uri: Uri = WALLETD_DEFAULT.parse().unwrap();
-    Factom{
-      client: new_client(),
-      factomd: request_builder(factomd_uri.clone()),
-      walletd: request_builder(walletd_uri.clone()),
-      factomd_uri,
-      walletd_uri,
-      id: 0
-    }
+  pub async fn factomd_call(self, req: ApiRequest) -> ResponseFuture {
+    self.inner_call(RequestType::Factomd, req).await
   }
 
-  pub async fn factomd_call(mut self, mut req: ApiRequest) -> ResponseFuture {
-    req.id = self.id;
-    let req = self.factomd.body(Body::from(req.json())).unwrap();
-    self.client.request(req)
-  }
-
-  pub async fn walletd_call(mut self, mut req: ApiRequest) -> ResponseFuture {
-    req.id = self.id;
-    let req = self.walletd.body(Body::from(req.json())).unwrap();
-    self.client.request(req)
+  pub async fn walletd_call(self, req: ApiRequest) -> ResponseFuture {
+    self.inner_call(RequestType::Walletd, req).await
   }
 
   pub async fn debug_call(self, req: ApiRequest) -> ResponseFuture {
-    // TODO: Replace factomd uri in contructors
-    self.inner(RequestType::Debug, req).await
+    self.inner_call(RequestType::Debug, req).await
   }
 
-  pub async fn inner(self, req_type: RequestType, mut req: ApiRequest) -> ResponseFuture {
+  async fn inner_call(
+    self, 
+    req_type: RequestType, 
+    mut req: ApiRequest
+  ) -> ResponseFuture 
+  {
     req.id = self.id;
-
     let mut node_request = match req_type {
       RequestType::Factomd => self.factomd,
       RequestType::Walletd => self.walletd,
-      RequestType::Debug => self.factomd
+      RequestType::Debug => self.debug
     };
     let json = Body::from(req.json());
     let req = node_request.body(json)
@@ -122,8 +79,10 @@ pub async fn parse<T>(fut: ResponseFuture) -> Result<ApiResponse<T>>
   where T: DeserializeOwned + Default
 {
   let res = fut.await?;
-  let body = res.into_body().try_concat().await?;
-  dbg!(&body);
+  let body = res.into_body()
+              .try_concat()
+              .await
+              .expect("Parsing response body");
   let res: ApiResponse<T> = deser(body).await;
   Ok(res)
 }
@@ -135,39 +94,14 @@ async fn deser<T>(body: Chunk) -> ApiResponse<T>
   serde_json::from_slice(&body).expect("Deserialising JSON")
 }
 
-// Creates a http client, this is placed in the Factom struct and is responsible
-// for making network requests
-fn new_client() -> HttpsClient {
-  let https = HttpsConnector::new().expect("TLS initialization");
-  Client::builder().build::<_, hyper::Body>(https)
+/// Fetch is a convenience function that will run a future to it's completion,
+/// the function will create a new runtime for every call, if making multiple
+/// api calls for synchronous usage for efficiency it's recommended to create 
+/// a single runtime and re-use it's blocking method instead
+pub fn fetch<F: Future>(query: F) -> F::Output {
+  let rt = Runtime::new().expect("Initialising Runtime");
+  rt.block_on(query)
 }
-
-// Builds the basis of a request minus the body, this is kept in the Factom
-// struct to avoid rebuilding the request everytime
-fn request_builder(uri: Uri) -> Builder {
-  let mut req = Request::builder();
-  req.method("POST")
-      .header(CONTENT_TYPE, "application/json")
-      .uri(uri);
-  req
-}
-
-/// Fetch is a convenience function to run a future to it's completion,  is a 
-/// wrapper  around futures::executor::block_on()
-pub fn fetch<T, F>(fut: F) ->  F::Output
-  where T: DeserializeOwned + Default,
-        F: Send + Future
-{
-  futures::executor::block_on(fut)
-}
-
-// #[derive(Deserialize, PartialEq, Default, Debug)]
-// pub struct Heights {
-//   directoryblockheight: usize,
-//   leaderheight: usize,
-//   entryblockheight: usize,
-//   entryheight: usize
-// }
 
 // #[cfg(test)]
 // mod tests {
