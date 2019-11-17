3e84a5ca427ff3807c455610db1775473f0953da
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-11-18 10:56:19 +1100

    doc refactoring

--- src/import.rs
+++ src/import.rs
@@ -3,19 +3,19 @@ use std::collections::HashMap;
 
 impl Factom {
   /**
-Import Factoid and/or Entry Credit address secret keys into the wallet.
-# Example
-```
-use factom::*;
-
-let addresses = vec!("Fs3E9gV6DXsYzf7Fqx1fVBQPQXV695eP3k5XbmHEZVRLkMdD9qCK");
-let factom = Factom::new();
-let query = factom
-            .import_addresses(addresses)
-            .map(|response| response).map_err(|err| err);
-let response = fetch(query).unwrap();
-assert!(response.success());  
-```
+/// Import Factoid and/or Entry Credit address secret keys into the wallet.
+/// # Example
+/// ```
+/// use factom::*;
+/// 
+/// let addresses = vec!("Fs3E9gV6DXsYzf7Fqx1fVBQPQXV695eP3k5XbmHEZVRLkMdD9qCK");
+/// let factom = Factom::new();
+/// let query = factom
+///             .import_addresses(addresses)
+///             .map(|response| response).map_err(|err| err);
+/// let response = fetch(query).unwrap();
+/// assert!(response.success());  
+/// ```
 */
   pub async fn import_addresses(
     self, 
@@ -35,23 +35,23 @@ assert!(response.success());
   }
 
   /**
-Allows a user to add one or more identity keys to the wallet. Using the secret 
-keys as input, the command will return the corresponding key pairs that were 
-imported. If the wallet is encrypted, it must be unlocked prior to using this 
-command.
-
-# Example
-```
-use factom::*;
-
-let addresses = vec!("idsec2rWrfNTD1x9HPPesA3fz8dmMNZdjmSBULHx8VTXE1J4D9icmAK");
-let factom = Factom::new();
-let query = factom
-            .import_identity_keys(addresses)
-            .map(|response| response).map_err(|err| err);
-let response = fetch(query).unwrap();
-assert!(response.success());  
-```
+/// Allows a user to add one or more identity keys to the wallet. Using the secret 
+/// keys as input, the command will return the corresponding key pairs that were 
+/// imported. If the wallet is encrypted, it must be unlocked prior to using this 
+/// command.
+/// 
+/// # Example
+/// ```
+/// use factom::*;
+/// 
+/// let addresses = vec!("idsec2rWrfNTD1x9HPPesA3fz8dmMNZdjmSBULHx8VTXE1J4D9icmAK");
+/// let factom = Factom::new();
+/// let query = factom
+///             .import_identity_keys(addresses)
+///             .map(|response| response).map_err(|err| err);
+/// let response = fetch(query).unwrap();
+/// assert!(response.success());  
+/// ```
 */
   pub async fn import_identity_keys(
     self, 
@@ -71,23 +71,23 @@ assert!(response.success());
   }
 
 /**
-Import a Koinify crowd sale address into the wallet. In our examples we used 
-the word “yellow” twelve times, note that in your case the master passphrase 
-will be different. If the wallet is encrypted, it must be unlocked prior to 
-using this command.
-
-# Example
-```
-use factom::*;
-
-let koinify_phrase = "yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow";
-let factom = Factom::new();
-let query = factom
-            .import_koinify(koinify_phrase)
-            .map(|response| response).map_err(|err| err);
-let response = fetch(query).unwrap();
-assert!(response.success());  
-```
+/// Import a Koinify crowd sale address into the wallet. In our examples we used 
+/// the word “yellow” twelve times, note that in your case the master passphrase 
+/// will be different. If the wallet is encrypted, it must be unlocked prior to 
+/// using this command.
+/// 
+/// # Example
+/// ```
+/// use factom::*;
+/// 
+/// let koinify_phrase = "yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow";
+/// let factom = Factom::new();
+/// let query = factom
+///             .import_koinify(koinify_phrase)
+///             .map(|response| response).map_err(|err| err);
+/// let response = fetch(query).unwrap();
+/// assert!(response.success());  
+/// ```
 */
   pub async fn import_koinify(
     self, 
