b24c1f05e9378d461044a8308803ce26e5521b76
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-11-20 10:24:39 +1100

    modified pub exports, added rc client to https type

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
+#![forbid(unsafe_code)]
 #![doc(html_logo_url = "https://seeklogo.com/images/F/factom-fct-logo-08400C829C-seeklogo.com.png",
  html_favicon_url = "https://www.factom.com/wp-content/uploads/2019/06/cropped-factom_favicon_azul-05-192x192.png",
  html_playground_url = "https://play.rust-lang.org/")]
-#![forbid(unsafe_code)]
-#![allow(dead_code, non_camel_case_types)]
 
 //! A rust client for the Factom network API.
 //!
@@ -27,21 +26,20 @@ pub mod requests;
 pub mod responses;
 
 pub use api::Factom;
-pub use factomd::*;
-pub use tokio::prelude::*;
-pub use tokio::runtime::Runtime;
-pub use hyper::rt::{Future, Stream};
-pub use serde_json::{Value, json};
-pub use serde::{Serialize, Deserialize};
 pub use constants::*; 
+pub use tokio::runtime::Runtime;
 pub use responses::ApiResponse;
 pub use requests::{ApiRequest, parse};
 
-use hyper_tls::HttpsConnector;
-use hyper::{Client, client::HttpConnector};
 
-/// Hyper client with custom https connector
-pub type HttpsClient = Client<HttpsConnector<HttpConnector>, hyper::Body>;
+use std::rc::Rc;
+use serde_json::json;
+use hyper_tls::HttpsConnector;
+use serde::{Serialize, Deserialize};
+use hyper::{Client, client::HttpConnector, rt::Future};
+
+/// Reference counted Hyper client with custom https connector
+pub type HttpsClient = Rc<Client<HttpsConnector<HttpConnector>, hyper::Body>>;
 
 /// Async return type for API Calls 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
