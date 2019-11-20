69065f9b53b0bd8edbc64abcc517911205908263
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-11-20 16:32:31 +1100

    add is_err function for ApiResponse

--- src/responses.rs
+++ src/responses.rs
@@ -1,4 +1,6 @@
 use super::*;
+use std::{error::Error, default::Default};
+use std::fmt::{self, Debug, Display};
 
 /// JSON responses are deserialized into this struct
 #[derive(Deserialize, Debug, PartialEq)]
@@ -10,12 +12,46 @@ pub struct ApiResponse<T>
   #[serde(default)]
   pub result: T,
   #[serde(default)]
-  pub error: Error
+  pub error: ApiError
 }
 
-// Generic Factom API Error struct
+/// Generic Factom API Error struct
 #[derive(Deserialize, PartialEq, Default, Debug)]
-pub struct Error {
+pub struct ApiError {
   pub code: i16,
   pub message: String
+}
+
+// impl<T> Display for  ApiResponse<T> 
+//   where T: Default + Debug + Display
+// {
+//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//       write!(f, "id: {}\nresult: {}\n error: {}", 
+//                   self.id, self.result, self.error)
+//   }
+// }
+
+// impl Display for  ApiError {
+//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//       write!(f, "{}", self)
+//   }
+// }
+
+// impl<T> Error for ApiResponse<T> 
+//   where T: Default + Debug + Display
+// {
+//   fn description(&self) -> &str {
+//     &self.error.message
+//   }
+// }
+
+impl<T> ApiResponse<T>
+  where T: Default
+{
+  /// Returns a boolean representing whether the api response returned an error
+  /// This function does not deal with network errors, that is handled earlier
+  /// by the Result from running the future query to completion.
+  pub fn is_err(&self) -> bool {
+    self.error.code != 0i16
+  }
 }
