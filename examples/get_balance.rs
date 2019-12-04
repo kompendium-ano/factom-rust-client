43c135e1e5c6cada4f4999ff1b4253055f4431aa
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-04 11:59:35 +1100

    get_balance example added

--- examples/get_balance.rs
+++ examples/get_balance.rs
@@ -2,14 +2,17 @@ use factom::*;
 
 const FCT_PUB: &str = "FA2jK2HcLnRdS94dEcU27rF3meoJfpUcZPSinpb7AwQvPRY6RL1Q";
 
-fn main() {
+#[tokio::main]
+async fn main() {
   let api = Factom::testnet_node();
   let query = api.factoid_balance(FCT_PUB);
-  let response = fetch(query).expect("Fetching query");
+  let response = query.await.expect("Fetching query");
   if response.is_err() {
     println!("Error: {}\nCode: {}", response.error.message, response.error.code);
   }
   else {
-    println!("The balance of {} is {} factoids", FCT_PUB, &response.result.balance);
+    // factoid balance returns factoshis
+    let factoids = *&response.result.balance as f64 / 100_000_000f64;
+    println!("The testnet balance of {} is {} factoids", FCT_PUB, factoids);
   }
 }
