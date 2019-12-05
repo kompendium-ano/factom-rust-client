2ca1e6ad2a9f41f4e64c58c0fba6486fc6b605ba
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-05 20:04:07 +1100

    moved builder into requests module

--- src/requests.rs
+++ src/requests.rs
@@ -6,7 +6,9 @@ use std::num::Wrapping;
 use futures_util::TryStreamExt;
 use crate::responses::ApiResponse;
 use serde::{Serialize, de::DeserializeOwned};
-use hyper::{Chunk, Body, client::ResponseFuture};
+use hyper::{Chunk, Body, Request, client::ResponseFuture};
+use http::{Uri, request::Builder, header::CONTENT_TYPE};
+
 
 /// Generic request struct is serialized into the JSON body 
 #[derive(Serialize, Debug, Clone)]
@@ -33,46 +35,45 @@ impl ApiRequest {
   pub fn json(self) -> String {
     serde_json::to_string(&self).expect("Serializing json")
   }
-}
 
-enum RequestType{
-  Factomd,
-  Walletd,
-  Debug
-}
-
-impl Factom {
-  pub async fn factomd_call(self, req: ApiRequest) -> ResponseFuture {
-    self.inner_call(RequestType::Factomd, req).await
-  }
-
-  pub async fn walletd_call(self, req: ApiRequest) -> ResponseFuture {
-    self.inner_call(RequestType::Walletd, req).await
-  }
-
-  pub async fn debug_call(self, req: ApiRequest) -> ResponseFuture {
-    self.inner_call(RequestType::Debug, req).await
-  }
-
-  async fn inner_call(
-    self, 
-    req_type: RequestType, 
-    mut req: ApiRequest
-  ) -> ResponseFuture 
-  {
-    req.id = self.id;
-    let node_request = match req_type {
-      RequestType::Factomd => self.factomd,
-      RequestType::Walletd => self.walletd,
-      RequestType::Debug => self.debug
-    };
-    let json = Body::from(req.json());
-    let req = node_request.borrow_mut()
-                          .body(json)
-                          .expect("Constructing request body");
-    self.client.request(req)
+  /// Builds the basis of a request minus the json body
+  pub fn builder(uri: &Uri) -> Builder {
+    let mut req = Request::builder();
+    req.method("POST")
+        .header(CONTENT_TYPE, "application/json")
+        .uri(uri);
+    req
   }
 }
+
+/// Makes a request to the current factomd node
+pub async fn factomd_call(api: &Factom, req: ApiRequest) -> ResponseFuture {
+  inner_call(api, &api.factomd_uri, req).await
+}
+
+/// Makes a request to the current walletd node
+pub async fn walletd_call(api: &Factom, req: ApiRequest) -> ResponseFuture {
+  inner_call(api, &api.walletd_uri, req).await
+}
+
+/// Makes a request to the current factomd node using the debug path 
+pub async fn debug_call(api: &Factom, req: ApiRequest) -> ResponseFuture {
+  inner_call(api, &api.debug_uri, req).await
+}
+
+async fn inner_call(
+  api: &Factom,
+  uri: &Rc<Uri>,
+  req: ApiRequest
+) -> ResponseFuture 
+{
+  let json = Body::from(req.json());
+  let mut builder = ApiRequest::builder(&uri);
+  let payload = builder.body(json)
+                        .expect("Constructing request body");
+  api.client.request(payload)
+}
+
 
 /// Parses the response and deserialises the API call into an appropriate 
 /// ApiResponse struct
@@ -81,14 +82,14 @@ pub async fn parse<T>(fut: ResponseFuture) -> Result<ApiResponse<T>>
 {
   let res = fut.await?;
   let body = res.into_body()
-              .try_concat()
-              .await
-              .expect("Parsing response body");
+                .try_concat()
+                .await
+                .expect("Parsing response body");
   let res: ApiResponse<T> = deser(body).await;
   Ok(res)
 }
 
-// Inner deserialisation method for parse_response 
+// Inner deserialisation method
 async fn deser<T>(body: Chunk) -> ApiResponse<T> 
   where T: DeserializeOwned + Default
 {
@@ -97,33 +98,36 @@ async fn deser<T>(body: Chunk) -> ApiResponse<T>
 
 /// Fetch is a convenience function that will run a future to it's completion,
 /// the function will create a new runtime for every call, if making multiple
-/// api calls for synchronous usage for efficiency it's recommended to create 
+/// api calls for synchronous usage it's recommended to create 
 /// a single runtime and re-use it's blocking method instead
 pub fn fetch<F: Future>(query: F) -> F::Output {
   let rt = Runtime::new().expect("Initialising Runtime");
   rt.block_on(query)
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//     #[test]
-//     fn heights() {
-//       let rt = Runtime::new().expect("Initialising Runtime");
-//       let api = Factom::new();
-//       let query = api.heights();
-//       let res = rt.block_on(query).expect("Runtime blocking thread");
-//       assert!(res.result.directoryblockheight > 1)
-//     }
+#[cfg(test)]
+mod tests {
+  // use super::*;
+    // #[test]
+    // fn heights() {
+    //   let rt = Runtime::new().expect("Initialising Runtime");
+    //   let api = Factom::open_node();
+    //   let query = api.heights();
+    //   let res = rt.block_on(query).expect("Runtime");
+    //   let query2 = api.properties();
+    //   let res2 = rt.block_on(query2).expect("Runtime");
+    //   dbg!(res2);
+    //   assert!(res.result.directoryblockheight > 1)
+    // }
 
-//     #[test]
-//     fn method_not_found() {
-//       let rt = Runtime::new().expect("Initialising Runtime");
-//       let api = Factom::new();
-//       let query = api.method_not_found();
-//       let res = rt.block_on(query).expect("Runtime blocking thread");
-//       assert_eq!(res.error.code, -32601);
-//     }
-// }
+    // #[test]
+    // fn method_not_found() {
+    //   let rt = Runtime::new().expect("Initialising Runtime");
+    //   let api = Factom::new();
+    //   let query = api.();
+    //   let res = rt.block_on(query).expect("Runtime blocking thread");
+    //   assert_eq!(res.error.code, -32601);
+    // }
+}
 
 
