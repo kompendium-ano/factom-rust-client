aa735bbc34a1d8ef6e8609b6601bf56a79861f07
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-10 14:27:53 +1100

    additional comments for endusers

--- benches/benches.rs
+++ benches/benches.rs
@@ -12,5 +12,8 @@ fn cloning_benchmark(c: &mut Criterion) {
     |b| b.iter(|| black_box(api.clone())));
 }
 
+// Benchmarks need to be added to the criterion group
 criterion_group!(benches, cloning_benchmark);
+
+// Criterion main runs the group benches
 criterion_main!(benches);
