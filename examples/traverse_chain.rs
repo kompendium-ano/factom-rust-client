4cbf155bd1bd99a287de18e32e0e96e4d8f2f719
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-12 12:55:52 +1100

    added traverse chain example

---
+++ examples/traverse_chain.rs
@@ -0,0 +1,28 @@
+use factom::{Factom, chain::chain_head, block::entry_block};
+
+// Transactions chain
+const PNET_TX_CHAIN: &str = "cffce0f409ebba4ed236d49d89c70e4bd1f1367d86402a3363366683265a242d";
+
+// This example partially traverses the pegnet conversion chain starting
+// from the most recent entries
+#[tokio::main]
+async fn main() {
+  let api = Factom::open_node();
+  let chainhead_response = chain_head(&api, PNET_TX_CHAIN)
+                            .await
+                            .expect("Fetching query");
+  let mut keymr = chainhead_response.result.chainhead;
+  let mut entries = Vec::new(); 
+  // It's a large chain, lets only traverse the last 5 blocks
+  for _ in 0..5u8 {
+    let response = entry_block(&api, &keymr)
+                            .await
+                            .expect("Fetching query");
+    keymr = response.result.header.prevkeymr;
+    let entrylist = response.result.entrylist;
+    for entry in entrylist {
+      entries.push(entry.entryhash);
+    }
+  }
+  dbg!(entries);
+}
