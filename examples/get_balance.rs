cc4144625f01941175f7cb8c719a4a88120e8ebf
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-12 14:58:59 +1100

    use factoshi conversion utility

--- examples/get_balance.rs
+++ examples/get_balance.rs
@@ -1,4 +1,4 @@
-use factom::{Factom, balance::factoid_balance};
+use factom::{Factom, balance::factoid_balance, utils::factoshis_to_fct};
 
 const FCT_PUB: &str = "FA2jK2HcLnRdS94dEcU27rF3meoJfpUcZPSinpb7AwQvPRY6RL1Q";
 
@@ -6,13 +6,13 @@ const FCT_PUB: &str = "FA2jK2HcLnRdS94dEcU27rF3meoJfpUcZPSinpb7AwQvPRY6RL1Q";
 async fn main() {
   let client = Factom::testnet_node();
   let query = factoid_balance(&client, FCT_PUB);
-  let response = query.await.expect("Fetching query");
+  let response = query.await.unwrap();
   if response.is_err() {
     println!("Error: {}\nCode: {}", response.error.message, response.error.code);
   }
   else {
     // factoid balance returns factoshis
-    let factoids = *&response.result.balance as f64 / 100_000_000f64;
+    let factoids = factoshis_to_fct(response.result.balance);
     println!("The testnet balance of {} is {} factoids", FCT_PUB, factoids);
   }
 }
